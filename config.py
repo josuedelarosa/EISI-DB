85d05d3c9a421a1d78a7ba08eeb2a2f8ced721c8
Josue <josue2200166@correo.uis.edu.co>
2024-11-30 23:52:53 -0500

    db deployment on render implemented

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = "postgresql://josue:2200166@localhost/eisi"
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    # SQLALCHEMY_DATABASE_URI = "postgresql://josue:2200166@localhost/eisi"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.urandom(24)  # Llave para proteger formularios
